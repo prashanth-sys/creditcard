ccc0cdd324ae6c67d7e6170ff8cc5dc60466f439
root <reddyprashanth512@gmail.com>
2024-07-08 14:27:50 +0000

    solution

---
+++ src/components/CreditCard/index.js
@@ -0,0 +1,22 @@
+// Write your code here
+
+import {
+  BgContainer,
+  CardContainer,
+  CardHeading,
+  Line,
+  CreditCardImageContainer,
+} from './styledComponents'
+
+const CreditCard = () => (
+  <BgContainer>
+    <CardContainer>
+      <CardHeading>CREDIT CARD</CardHeading>
+      <Line />
+      <CreditCardImageContainer>
+        <p>credit card</p>
+      </CreditCardImageContainer>
+    </CardContainer>
+  </BgContainer>
+)
+export default CreditCard
---
+++ src/components/CreditCard/styledComponents.js
@@ -0,0 +1,32 @@
+// Style your elements here
+
+import styled from 'styled-components'
+
+export const BgContainer = styled.div`
+  background-color: #3b4b69;
+  background-size: cover;
+  height: 100vh;
+  width: 50%;
+`
+export const CardContainer = styled.div`
+  text-align: center;
+  color: #ffffff;
+  font-family: 'Roboto';
+`
+
+export const CardHeading = styled.h1`
+  font-size: 25px;
+  padding-top: 25px;
+`
+
+export const Line = styled.hr`
+  background-color: #ffd773;
+  width: 120px;
+  height: 2px;
+  border-width: 0px;
+`
+export const CreditCardImageContainer = styled.div`
+  background-image: url('https://assets.ccbp.in/frontend/hooks/credit-card-bg.png');
+  height: 100px;
+  width: 50%;
+`
